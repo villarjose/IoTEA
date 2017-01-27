95b9d659361ff07f766744e6830109edfb330f35
villarjose <villarjose.uniovi@gmail.com>
2017-01-27 13:45:43 +0100

    SMA function in acc_functions is finished

--- lib/ptnlib/acc_functions.py
+++ lib/ptnlib/acc_functions.py
@@ -5,6 +5,8 @@ Created on Fri Jan 27 11:28:15 2017
 
 @author: villarjose
 """
+import numpy as np
+import pandas as pd
 
 def sma(x):
     """Function sma(x)
@@ -18,4 +20,12 @@ Parameters:
 Returns:
     a numpy vector of dimension 1xm
     
+    
+For instance,if you have an ACCdf object, you can call sma using the 
+get_components method:
+    >>> a = ACCdf(myDataFrame) #this is from a accelerometer
+    >>> sma_a = sma(a.get_components())
+    
     """
+    r = np.fromiter(map(lambda x: sum(abs(x)), p), np.float)
+    return sum(r)/len(r)
