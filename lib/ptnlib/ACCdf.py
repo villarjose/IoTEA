bdc201c66be47e8f3d031fd0797335001f0bbacf
Jose Ramon Villar <villarjose.uniovi@gmail.com>
2017-02-03 14:46:23 +0100

    Feb 3, 2017: aom and sma

--- lib/ptnlib/ACCdf.py
+++ lib/ptnlib/ACCdf.py
@@ -4,10 +4,45 @@
 Created on Fri Jan 27 11:35:49 2017
 
 @author: villarjose at uniovi.es
+
 """
+
+
 import numpy as np
 import pandas as pd
 
+
+
+"""
+##############################################################
+class ACCdf(pd.DataFrame):
+This class derives from pandas.DataFrame, and its aim is to
+determine if a given sequence is a tri-axial-accelerometer 
+time series: that is, we may have one of the three following
+scenarios:
+   <x>-axis + class, so we have a mx2 DataFrame
+   <x,y>-axis + class, so we have a mx3 DataFrame
+   <x,y,z>-axis + class, so we have a mx4 DataFrame
+   
+The test method generates an Exception when the DataFrame does
+not match with one of the previous options.
+
+To create one ACCdf we use the constructor:
+>>> d = pd.DataFrame([[2,0],[3,1],[4,0]])
+>>> a = ACCdf(d) #if wrong, an Exception arises
+
+To extract the components from the data -that is, forgetting 
+about the class values-, you can use the get_components method:
+>>> npa = a.get_components() #npa is a np.array
+
+To obtain the class values, use the get_class method:
+>>> c = a.get_class()  #c is a np.array
+
+In all the cases, the returned arrays are COPIED from the 
+original data!!!! Tha is, both get_components and get_class
+method create a copy of the data.
+
+"""
 class ACCdf(pd.DataFrame):
     'A pandas.DataFrame for managing 3D acceleration samples'
     
@@ -36,3 +71,101 @@ class ACCdf(pd.DataFrame):
         return np.copy(v)
 
 
+    
+"""
+##############################################################
+class ACC_BA_Gdf(pd.DataFrame):
+This class derives from pandas.DataFrame, and its aim is to
+determine if a given sequence might include the tri-axial
+accelerometer values from a sensor plus the Body Acceleration
+and the Gravity acceleration -that should have been previously
+filtered-. So, the incoming DataFrame should have dimension 
+   m x (12 + 1)
+with m the number of samples, 12 comes from: 3 for ACC, 3 for 
+BA and 3 for G, and 3 comes from the modulus of ACC, BA and G; 
+finally, the 1 comes from the class. 
+   
+The test method generates an Exception when the DataFrame does
+not match with the previous dimension.
+
+To create one ACC_BA_Gdf we use the constructor:
+>>> d = pd.DataFrame([[2,0],[3,1],[4,0]]) #expecting a m x 13 DataFrame
+>>> a = ACC_BA_Gdf(d) #if wrong, an Exception arises
+
+To extract the components from the data -that is, forgetting 
+about the class values-, you can use the get_components method.
+You must specify the acceleration you wish to obtain:
+    ACC_BA_Gdf.acc()  for the three components of ACC
+    ACC_BA_Gdf.ba()   for the three components of BA
+    ACC_BA_Gdf.g()    for the three components of G
+    ACC_BA_Gdf.ACC()  for the modulus of ACC  
+    ACC_BA_Gdf.BA()   for the modulus of BA   
+    ACC_BA_Gdf.G()    for the modulus of G  
+>>> npa = a.get_components(ACC_BA_Gdf.BA()) #npa is a np.array
+
+To obtain the class values, use the get_class method:
+>>> c = a.get_class()  #c is a np.array
+
+In all the cases, the returned arrays are COPIED from the 
+original data!!!! Tha is, both get_components and get_class
+method create a copy of the data.
+
+"""
+class ACC_BA_Gdf(pd.DataFrame):
+    'A pandas.DataFrame for managing 3D acceleration samples'
+    
+    @staticmethod
+    def acc():
+        return 1
+    @staticmethod
+    def ba():
+        return 2
+    @staticmethod
+    def g():
+        return 4
+    @staticmethod
+    def ACC():
+        return 8
+    @staticmethod
+    def BA():
+        return 16
+    @staticmethod
+    def G():
+        return 32
+    @staticmethod
+    def ALL():
+        return 64
+        
+    def test(self):
+        print(self.shape[1], self.shape)
+        if self.shape[1] != 13:
+            raise Exception("not a ACC+BA+G+Class acceleration signal")
+        
+    
+    def __init__(self, *args, **kwargs):
+        super(ACCdf, self).__init__(*args, **kwargs)
+        self.test()
+    
+    def get_components(self,comp):
+        if comp == self.acc():
+            v = self.values[:,0:3]
+        elif comp == self.ba():
+            v = self.values[:,3:6]
+        elif comp == self.g(): 
+            v = self.values[:,6:9]
+        elif comp == self.ACC(): 
+            v = self.values[:,9]
+        elif comp == self.BA(): 
+            v = self.values[:,10]
+        elif comp == self.G(): 
+            v = self.values[:,11]
+        elif comp == self.ALL():
+            v = self.values[:,0:-1]
+        return np.copy(v)
+    
+    def get_class(self):
+        v = self.values[:,-1]
+        return np.copy(v)
+    
+    
+        
--- lib/ptnlib/acc_functions.py
+++ lib/ptnlib/acc_functions.py
@@ -10,22 +10,84 @@ import pandas as pd
 
 def sma(x):
     """Function sma(x)
-    Computes the Signal Amount Ratio as the sum of the absolute values of its 
+    Computes the Signal Magnitude Area as the sum of the absolute values of its 
     components in each dimension.
 
 Sintax:
     sma(x)
 Parameters:
-    x: a numpy array of dimension mxn
+    x: a numpy array of dimension mxn, with n in {1, 2, 3} or n>=9
 Returns:
-    a numpy vector of dimension 1xm
+    a numpy vector of dimension 1xm or mx3
     
     
 For instance,if you have an ACCdf object, you can call sma using the 
 get_components method:
     >>> a = ACCdf(myDataFrame) #this is from a accelerometer
     >>> sma_a = sma(a.get_components())
-    
+
+And if you have an ACC_BA_Gdf object, you can call sma using the get_components 
+method:
+    >>> a = ACC_BA_Gdf(myDataFrame) #this is from a accelerometer
+    >>> sma_a = sma(a.get_components())
+In this latter case, SMA is computed for the acc, ba, and g components, but not 
+for the modulus of these accelerations -ACC, BA and G-. Therefore, the output 
+is an array of mx3 -<sma(acc), sma(ba), sma(g)>-.
+
     """
-    r = np.fromiter(map(lambda x: sum(abs(x)), p), np.float)
-    return sum(r)/len(r)
+    if x.shape[1] <= 3: #case of ACCdf
+        r = np.fromiter(map(lambda v: sum(abs(v)), x), np.float)
+        return np.sum(r)/len(r)
+    elif x.shape[1] >=9: #case of ACC_BA_Gdf
+        acc = np.fromiter(map(lambda v: sum(abs(v)), x[:,0:3]), np.float)
+        ba = np.fromiter(map(lambda v: sum(abs(v)), x[:,3:6]), np.float)
+        g = np.fromiter(map(lambda v: sum(abs(v)), x[:,6:9]), np.float)
+        r = np.stack((acc,ba,g),axis=1)
+        xlen = max(1, x.shape[0])
+        return np.sum(r)/xlen
+    else:
+        raise Exception("SMA: Invalid number of columns of the data array.");
+
+
+def aom(x):
+    """Function aom(x)
+    Computes the Amount of Movement as the sum of the differences between
+the maximum and the minimum values in the different axis of a sample.    
+
+Sintax:
+    aom(x)
+Parameters:
+    x: a numpy array of dimension mxn, with n in {1, 2, 3} or n>=9
+Returns:
+    a numpy vector of dimension 1xm or mx3
+    
+    
+For instance,if you have an ACCdf object, you can call sma using the 
+get_components method:
+    >>> a = ACCdf(myDataFrame) #this is from a accelerometer
+    >>> sma_a = aom(a.get_components())
+    
+And if you have an ACC_BA_Gdf object, you can call sma using the get_components 
+method:
+    >>> a = ACC_BA_Gdf(myDataFrame) #this is from a accelerometer
+    >>> sma_a = aom(a.get_components())
+In this latter case, SMA is computed for the acc, ba, and g components, but not 
+for the modulus of these accelerations -ACC, BA and G-. Therefore, the output 
+is an array of mx3 -<sma(acc), sma(ba), sma(g)>-.
+
+    """
+    if x.shape[1] <= 3: #case of ACCdf
+        r = np.abs(np.max(x, axis=1) - np.min(x, axis=1))
+        return np.sum(r)
+    elif x.shape[1] >=9: #case of ACC_BA_Gdf
+        acc = np.abs(np.max(x[:, 0:3], axis=1) - np.min(x[:, 0:3], axis=1))
+        ba = np.abs(np.max(x[:, 3:6], axis=1) - np.min(x[:, 3:6], axis=1))
+        g = np.abs(np.max(x[:, 6:9], axis=1) - np.min(x[:, 6:9], axis=1))
+        return np.sum(np.stack((acc,ba,g),axis=1), axis=1)
+    else:
+        raise Exception("AOM: Invalid number of columns of the data array.");
+
+        
+        
+        
+        
