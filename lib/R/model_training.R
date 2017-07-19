f750b44b5ede2cad0d64606d4662a70d98ecef7f
Jose Ramon Villar <villarjose.uniovi@gmail.com>
2017-07-19 13:54:05 +0200

    2017-07-18 before introducing crossvalidation and model learning

--- lib/R/model_training.R
+++ lib/R/model_training.R
@@ -2,7 +2,70 @@ library(RMySQL)
 library(signal)
 
 
+Qs <- function(s){
+  return(paste("'",s,"'",sep=''))
+}
 
+
+#------------------------------------------------------------------------------
+# setNaN(v, mu)
+#------------------------------------------------------------------------------
+#Ajusta los valores NaN en v al valor dado mu
+#Input:
+# v        vector a elimiar los NaN
+# mu       valor que sustituye al NaN
+#Output:
+#  v       vector sin NaN
+#
+setNaN <- function(v, mu) {
+  v[is.nan(v)] <- mu
+  return(v)
+}
+
+#------------------------------------------------------------------------------
+# UTILmeanNaN(v)
+#------------------------------------------------------------------------------
+#Calcula la media a una matriz eliminando los NaN
+#Input:
+# v        vector o matriz de datos
+#Output:
+# mu       media calculada
+#
+UTILmeanNaN <- function(v) {
+  q <- lapply(seq(1,ncol(v)), function(x) mean(v[!is.nan(v[,x]),x]))
+  result = unlist(q)
+  return(result)
+}
+
+#------------------------------------------------------------------------------
+# UTILsdNaN(v)
+#------------------------------------------------------------------------------
+#Calcula la sd a una matriz eliminando los NaN
+#Input:
+# v        vector o matriz de datos
+#Output:
+# mu       sd calculada
+#
+UTILsdNaN <- function(v) {
+  q <- lapply(seq(1,ncol(v)), function(x) sd(v[!is.nan(v[,x]),x]))
+  result = unlist(q)
+  return(result)
+}
+
+
+#------------------------------------------------------------------------------
+# DBconnectToHARDB(u = 'essy', p = 'Papatolati666', 
+#                  dbn = 'ESSYDB', h = '156.35.22.10')
+#------------------------------------------------------------------------------
+#Calcula la sd a una matriz eliminando los NaN
+#Input:
+# u        user in the database
+# p        password for u in the database
+# dbn      name of the database
+# h        host ip or domain name
+#Output:
+# connect  the link to the database
+#
 DBconnectToHARDB <- function(u = 'essy', p = 'Papatolati666', 
                              dbn = 'ESSYDB', h = '156.35.22.10') {
   connect <- dbConnect(MySQL(), user=u, password=p, dbname=dbn, host=h)
@@ -54,11 +117,12 @@ HARgetAllParticipantss <- function(connect, profile = 0) {
   if (profile == 0) { 
     sqlstm <- "select idparticipant, idprofile from participant; " 
   }
-  else if (level > 0) { 
+  else if (profile > 0) { 
     sqlstm <- sprintf("select idparticipant, idprofile from participant where idprofile=%d; ", profile)
   }
   else { return( data.frame() )}
   res <- dbGetQuery(connect, sqlstm)
+  return(res)
 }
 
 #tmp <- sprintf("SELECT * FROM emp WHERE lname = %s", "O'Reilly")
@@ -70,6 +134,19 @@ HARgetAllParticipantss <- function(connect, profile = 0) {
 #rs <- dbSendQuery(con, sql)
 #dbClearResult(rs)
 
+
+HARgetSlidingWindow <- function(connect, idprofile, idactivity = '0.0.0', idparticipant = -1){
+  if (idparticipant == -1) {
+    sqlstm <- sprintf("select windowsize, shift from sliding_window where idact1=%s and idprofile=%d;",
+                      Qs(idactivity), idprofile)
+  }
+  else {
+    sqlstm <- sprintf("select windowsize, shift from sliding_window where idact1=%s and  idprofile=%d and idparticipant=%d;",
+                      Qs(idactivity), idprofile, idparticipant)
+  }
+  res <- dbGetQuery(connect, sqlstm)
+  return(res)
+}
 
 
 #------------------------------------------------------------------------------
@@ -84,12 +161,14 @@ HARgetAllParticipantss <- function(connect, profile = 0) {
 #   una matriz de similitudes, pares <idactivity, <q25, q50, q75> >
 #
 HARcomputeActivitySimilarities <- function(connect, idactivity, idprofile=-1, idparticipant=-1 ) {
-  sqlstm <- paste('select accx,accy, accz, hr from data where idactivity =', idactivity)
+  sqlstm <- sprintf('select accx,accy, accz, hr from data where idactivity =%s', Qs(idactivity))
   if (profile != -1) {
-    sqlstm <- paste(sqlstm, 'and', 'idprofile =', toString(idprofile))
+    sqlstm <- paste(sqlstm, ' and idprofile =%d')
+    sqlstm <- sprintf(sqlstm, idprofile)
   }
   if (idparticipant != -1) {
-    sqlstm <- paste(sqlstm, 'and', 'idparticipant =', toString(idparticipant))
+    sqlstm <- paste(sqlstm, ' and idparticipant =%d' )
+    sqlstm <- sprintf(sqlstm, idparticipant)
   }
   sqlstm <- paste(sqlstm, ";")
   actdata <- dbGetQuery(connect, sqlstm)
@@ -102,7 +181,7 @@ HARcomputeActivitySimilarities <- function(connect, idactivity, idprofile=-1, id
   sims <- data.frame()
   for( idact in acts) {
     eudist <- data.frame()
-    sqlstm <- paste('select accx, accy, accz, hr from data where idactivity =', idact, ";")
+    sqlstm <- sprintf('select accx, accy, accz, hr from data where idactivity =%s;', Qs(idact))
     idactdata <- dbGetQuery(connect, sqlstm)
     whole <- rbind(actdata,idactdata)
     dm <- as.matrix(dist(whole, method = 'euclidean'))
@@ -119,7 +198,7 @@ HARcomputeActivitySimilarities <- function(connect, idactivity, idprofile=-1, id
 #------------------------------------------------------------------------------
 # HARsetActivitySimilarities
 #------------------------------------------------------------------------------
-#Esta función actualiza en la base de datos las similitudes de una actividad 
+#Esta funci??n actualiza en la base de datos las similitudes de una actividad 
 #con el resto de actividades del mismo contexto (idprofile, idparticipant)
 #Input:
 #   connect       acceso a la base de datos
@@ -135,10 +214,10 @@ HARsetActivitySimilarities <- function(connect, idactivity, idprofile = -1, idpa
   }
   basesql <- paste("insert into activity_distances",
                   "(idact1, idact2, idprofile, idparticipant, q25, q50, q75)",
-                  "values (%d, %d, %d, %d, %f, %f, %f);" )
+                  "values (%s, %s, %d, %d, %f, %f, %f);" )
   rns <- rownames(sims)
   for( r in rns) {
-    sqlstm <- sprintf(basesql, idactivity, r, idprofile, idparticipant, 
+    sqlstm <- sprintf(basesql, Qs(idactivity), Qs(r), idprofile, idparticipant, 
                       sims[r,1], sims[r,2], sims[r,3])
     res <- dbGetQuery(sqlstm)
   }
@@ -161,12 +240,14 @@ HARsetActivitySimilarities <- function(connect, idactivity, idprofile = -1, idpa
 #  ans     listado de actividades similares, con idactivity, y los 3 cuartiles.
 #
 HARgetActivitySimilarities <- function(connect, idactivity, idprofile = -1, idparticipant = -1) {
-  sqlstm <- paste('select idact2, q25, q50, q75 from activity_distances where idact1 =', idactivity)
+  sqlstm <- sprintf('select idact2, q25, q50, q75 from activity_distances where idact1 =%s', Qsi(dactivity))
   if (profile != -1) {
-    sqlstm <- paste(sqlstm, 'and', 'idprofile =', toString(idprofile))
+    sqlstm <- paste(sqlstm, ' and idprofile =%d')
+    sqlstm <- sprintf(sqlstm, idprofile)
   }
   if (idparticipant != -1) {
-    sqlstm <- paste(sqlstm, 'and', 'idparticipant =', toString(idparticipant))
+    sqlstm <- paste(sqlstm, 'and', 'idparticipant =%d')
+    sqlstm <- sprintf(sqlstm, idparticipant)
   }
   sqlstm <- paste(sqlstm, ';')
   actdata <- dbGetQuery(connect, sqlstm)
@@ -227,12 +308,12 @@ HARsplitIntoTS <- function(df) {
 #
 HARrequestAllACCHRData <- function(connect, idparticipant, idactivity="0.0.0") {
   if (idactivity == "0.0.0") {
-    sqlstm <- paste("select * from data where idparticipant = ", idparticipant,sep='')
+    sqlstm <- sprintf("select * from data where idparticipant = %d;", idparticipant)
     df <- dbGetQuery(sqlstm)
   }
   else {
-    sqlstm <- paste("select * from data where idparticipant = ", idparticipant,
-                    " and idactivity=",idactivity,sep='')
+    sqlstm <- sprintf("select * from data where idparticipant = %d and idactivity=%s", 
+                      idparticipant, Qs(idactivity))
     df <- dbGetQuery(sqlstm)
   }
   return(df)
@@ -258,7 +339,7 @@ HARrequestACCHRData <- function(connect, idparticipant, idactivity="0.0.0") {
   }
   else {
     sqlstm <- sprintf("select  time, accx, accy, accz, hr from data where idparticipant=%d and idactivity=%d;",
-                      idparticipant,idactivity)
+                      idparticipant, Qs(idactivity))
     df <- dbGetQuery(sqlstm)
   }
   return(df)
@@ -343,6 +424,7 @@ tbp <- function(d, K=0.9){
   return(g)
 }
 
+
 HARcomputeTBP <- function(df, ws, shift, K=0.9) {
   b <- seq(ws, nrow(df), shift)
   a <- seq(1, nrow(df) -shift, shift)
@@ -354,10 +436,34 @@ HARcomputeTBP <- function(df, ws, shift, K=0.9) {
 
 
 
+
+#------------------------------------------------------------------------------
+# HARcomputeHRmean(df, ws, shift)
+#------------------------------------------------------------------------------
+#Esta funcion calcula HRmean de un vector con HR. A sliding window 
+#is performed on the data.
+#Input:
+#  d      vector de tres coordanadas x,y,z
+#  ws     the sliding window size
+#  shift  the shift used for the sliding window
+#Output:
+#  HRmean calculado
+#
+HARcomputeHRmean <- function(df, ws, shift) {
+  b <- seq(ws, nrow(df), shift)
+  a <- seq(1, nrow(df) -shift, shift)
+  a <- a[1:nrow(b)]
+  r <- apply(data.frame(a, b), MARGIN=1,
+             function(x) mean(df[x[1]:x[2]]))
+  return(r)
+}
+
+
+
 #------------------------------------------------------------------------------
 # HARcreateEllipFilter(n, Rs, Rp, W, type='high', plane='z')
 #------------------------------------------------------------------------------
-#Esta funcion calcula un filtro usando el paquete signal, función ellip.
+#Esta funcion calcula un filtro usando el paquete signal, funci??n ellip.
 #Input:
 #  n, Rs, Rp, W      see signal::ellip for info
 #  type              'high', 'low', ...  see signal::ellip for info
@@ -463,50 +569,46 @@ HARextractGfromACC <- function(acc) {
 }
 
 
-
 #------------------------------------------------------------------------------
-# setNaN(v, mu)
+# HARcomputeBATransformations(df, ws, shift)
 #------------------------------------------------------------------------------
-#Ajusta los valores NaN en v al valor dado mu
+#Esta funcion calcula SMA, AoM & TBP, meanHR para un dataframe o matriz   con
+#las 3 componentes de aceleracion BA: x,y,z y el HR. Orden: nsamples x 4 columnas
 #Input:
-# v        vector a elimiar los NaN
-# mu       valor que sustituye al NaN
+#  d      vector de tres coordanadas x,y,z de la BA
+#  ws     the sliding window size
+#  shift  the shift used for the sliding window
 #Output:
-#  v       vector sin NaN
+#  res    matriz con 4 columnas: SMA, AoM, TBP, HRmean
 #
-setNaN <- function(v, mu) {
-  v[is.nan(v)] <- mu
-  return(v)
+HARcomputeBATransformations <- function( ldata, ws, shift) {
+  ba <- HARextractBAfromACC(l[[x]][,1:3])
+  c1 <- HARcomputeSMA(ba, ws, shift)
+  c2 <- HARcomputeAoM(ba, ws, shift)
+  c3 <- HARcomputeTBP(ba, ws, shift)
+  c4 <- HARcomputeHRmean(ldata[,4], ws, shift)
+  return(cbind(c1,c2,c3,c4))
 }
 
 #------------------------------------------------------------------------------
-# UTILmeanNaN(v)
+# HARcomputeGTransformations(df, ws, shift)
 #------------------------------------------------------------------------------
-#Calcula la media a una matriz eliminando los NaN
+#Esta funcion calcula SMA, AoM & TBP, meanHR para un dataframe o matriz   con
+#las 3 componentes de aceleracion G: x,y,z y el HR. Orden: nsamples x 4 columnas
 #Input:
-# v        vector o matriz de datos
+#  d      vector de tres coordanadas x,y,z de la G
+#  ws     the sliding window size
+#  shift  the shift used for the sliding window
 #Output:
-# mu       media calculada
+#  res    matriz con 4 columnas: SMA, AoM, TBP, HRmean
 #
-UTILmeanNaN <- function(v) {
-  q <- lapply(seq(1,ncol(v)), function(x) mean(v[!is.nan(v[,x]),x]))
-  result = unlist(q)
-  return(result)
-}
-
-#------------------------------------------------------------------------------
-# UTILsdNaN(v)
-#------------------------------------------------------------------------------
-#Calcula la sd a una matriz eliminando los NaN
-#Input:
-# v        vector o matriz de datos
-#Output:
-# mu       sd calculada
-#
-UTILsdNaN <- function(v) {
-  q <- lapply(seq(1,ncol(v)), function(x) sd(v[!is.nan(v[,x]),x]))
-  result = unlist(q)
-  return(result)
+HARcomputeGTransformations <- function( ldata, ws, shift) {
+  ba <- HARextractGfromACC(l[[x]][,1:3])
+  c1 <- HARcomputeSMA(ba, ws, shift)
+  c2 <- HARcomputeAoM(ba, ws, shift)
+  c3 <- HARcomputeTBP(ba, ws, shift)
+  c4 <- HARcomputeHRmean(ldata[,4], ws, shift)
+  return(cbind(c1,c2,c3,c4))
 }
 
 
@@ -551,20 +653,79 @@ MLsetActivitiesSimilaritiesbyParticipantActivity <- function(conn){
 
 
 
-MLsetActivitiesSimilaritiesbyParticipantActivity <- function(conn){
+MLinitializeAllML <- function(conn){
   parts <- HARgetAllParticipantss(conn) #column 1: idparticipant, column 2: idprofile
   acts <- HARgetAllActivities(conn, level = 3) #column 1: idactivity, column 2: label
-  for(i in 1:nrow(parts)){
-    for(a in 1:nrow(acts)) {
+  for(i in 1:nrow(parts)){ #for each participant i
+    #
+    #get the sliding window
+    window <- HARgetSlidingWindow(idprofile = parts[i,2])
+    for(a in 1:nrow(acts)) { #for each activity a
+      #
+      #obtain the activities similarities wrt a
       sims <- HARgetActivitySimilarities(conn, acts[a,1], idparticipant=parts[i,1], 
                                          idprofile=parts[i,2])
+      #sort the similarities according to the median
       ssims <- sims[with(sims, sort(sims$q50)),]
       simmean <- mean(sims$q50)
+      #
+      #obtain the data
+      #data for participant i and activity a 
       actdata <- HARrequestACCHRData(conn,idparticipant = parts[i,1], idactivity = acts[a,1])
-      mu = UTILmeanNaN(actdata)
-      sigma <- UTILsdNaN(actdata)
-      simAct <- rownames(ssims)[ssims<simean] #similar activities to current one
-      disimAct <- ronames(ssims)[ssims>=simean] #dissimilar activities to current one
+      muACC = UTILmeanNaN(actdata)
+      sigmaACC <- UTILsdNaN(actdata)
+      #data for participant i and the similar activities
+      #   a SIMILAR activity is that with distance SMALLER than the similarity distance mean simmean
+      simAct <- rownames(ssims)[ssims<simean] #similar activities to a
+      lsimdata <- lapply(seq(1,length(simAct)), function(x) HARrequestACCHRData(conn, idparticipant = parts[i,1], 
+                                                                                idactivity = simAct[x])) 
+      simdata <- do.call(rbind, lsimdata) #from a list of matrices to a matrix
+      #data for participant i and the dissimilar activities
+      #   a DISSIMILAR activity is that with distance EQUAL or HIGHER than the similarity distance mean simmean
+      disimAct <- ronames(ssims)[ssims>=simean] #dissimilar activities to a
+      ldsimdata <- lapply(seq(1,length(disimAct)), function(x) HARrequestACCHRData(conn, idparticipant = parts[i,1], 
+                                                                                 idactivity = disimAct[x]))
+      dsimdata <- do.call(rbind, ldsimdata)
+      #
+      #Now, its time to filter 
+      #   First: from a matrix to a set of TS
+      #   Second: fliter each TS, BUT ONLY THE ACC values!
+      #   Third: compute the SMA, Aom and TBP for the BA
+      #   Third: rearrange all of the BA and G filtered TS
+      #   Fourth: include the HR to the BA and G datasets!!! But without the timestamp
+      #for actdata
+      l <- HARsplitIntoTS(actdata)
+      LBA <- lapply(seq(1,length(l)), function(x) HARcomputeBATransformations(l[[x]][,2:5]), window.windowsize, window.shift)
+      LG <-  lapply(seq(1,length(l)), function(x) HARextractGfromACC(l[[x]][,2:4]) )
+      actBA <- cbind(do.call(rbind, LBA)) #<<--- without timestamp
+      actG <- cbind( do.call(rbind, LG), actdata[,5]) #<<--- without timestamp
+      #for simdata
+      l <- HARsplitIntoTS(simdata)
+      LBA <- lapply(seq(1,length(l)), function(x) HARcomputeBATransformations(l[[x]][,2:5]), window.windowsize, window.shift)
+      LG <-  lapply(seq(1,length(l)), function(x) HARextractGfromACC(l[[x]][,2:4]) )
+      simBA <- cbind(do.call(rbind, LBA)) #<<--- without timestamp
+      simG <- cbind( do.call(rbind, LG), simdata[,5]) #<<--- without timestamp
+      simtTRF <- HARcomputeTransformations(actBA, window.windowsize, window.shift)
+      #for dsimdata
+      l <- HARsplitIntoTS(dsimdata)
+      LBA <- lapply(seq(1,length(l)), function(x) HARcomputeBATransformations(l[[x]][,2:4]), window.windowsize, window.shift)
+      LG <-  lapply(seq(1,length(l)), function(x) HARextractGfromACC(l[[x]][,2:4]) )
+      dsimBA <- cbind(do.call(rbind, LBA)) #<<--- without timestamp
+      dsimG <- cbind(do.call(rbind, LG), dsimdata[,5]) #<<--- without timestamp
+      #
+      #Its time to pre-process the data:
+      #   to normalize BA according to mu and sigma for actdata
+      #   eliminate any NaN as 20*sigmaBA
+      #   
+      muBA <- UTILmeanNaN(actBA)
+      sigmaBA <- UTILsdNaN(actBA)
+      actBAn <- (actBA - muBA)/sigmaBA
+      actBAn[is.nan(actBAn)] = 20*sigmaBA
+      simBAn <- (simBA - muBA)/sigmaBA
+      simBAn[is.nan(simBAn)] = 20*sigmaBA
+      dsimBAn <- (dsimBA - muBA)/sigmaBA
+      dsimBAn[is.nan(dsimBAn)] = 20*sigmaBA
+      #
       
     }
   }
